455c23e06901288e2718e740f3121eeefcbedbc5
Vitalii Yarema <vyarema@windowslive.com>
2017-12-20 20:43:34 +0200

    Done

--- GroupPolicyLab5/MainWindow.xaml.cs
+++ GroupPolicyLab5/MainWindow.xaml.cs
@@ -31,16 +31,20 @@ namespace GroupPolicyLab5
 
         private async void buttonFile_Click(object sender, RoutedEventArgs e)
         {
-            var fileName = textBoxFile.Text.Trim();
-            if (string.IsNullOrEmpty(fileName))
-                return;
+            try
+            {
+                // Read file
+                await ReadFileAsync();
 
-            await ReadFileAsync();
+                await CalcNumEntry();
 
-            CalcNumEntry();
+                // Save to new file
+                await WriteFileAsync();
+            }
+            catch
+            {
 
-            // Save to new file
-            await WriteFileAsync(fileName);
+            }
         }
 
         private async Task ReadFileAsync()
@@ -57,48 +61,67 @@ namespace GroupPolicyLab5
                     }
                 }  
             }
-            catch (Exception exc)
+            catch 
             {
-                //ResultBlock.Text = "Could not read the file";
             }
         }
 
-        private async Task WriteFileAsync(string fileName)
+        private async Task WriteFileAsync()
         {
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.Filter = "Text files (*.txt)|*.txt";
-            saveFileDialog.DefaultExt = "txt";
-            saveFileDialog.AddExtension = true;
-            saveFileDialog.FileName = fileName;
-            if (saveFileDialog.ShowDialog() == true)
+            try
             {
-                using (StreamWriter outputFile = new StreamWriter(saveFileDialog.FileName))
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Text files (*.txt)|*.txt";
+                saveFileDialog.DefaultExt = "txt";
+                saveFileDialog.AddExtension = true;
+                saveFileDialog.FileName = "result";
+                if (saveFileDialog.ShowDialog() == true)
                 {
-                    int number = 0;
-                    foreach (var pair in _dictionary)
+                    using (StreamWriter outputFile = new StreamWriter(saveFileDialog.FileName))
                     {
-                        await outputFile.WriteLineAsync(pair.Key + " - " + pair.Value);
+                        int number = 0;
+                        foreach (var pair in _dictionary)
+                        {
+                            await outputFile.WriteLineAsync(pair.Key + " - " + pair.Value);
+                        }
+                        number = _dictionary.Sum(r => r.Value);
+                        await outputFile.WriteLineAsync("Total number - " + number);
                     }
-                    number = _dictionary.Sum(r => r.Value);
-                    await outputFile.WriteLineAsync("Total number - " + number);
                 }
+            }
+            catch
+            {
+
             }
         }
 
-        private void CalcNumEntry()
+        private async Task CalcNumEntry()
         {
-            // Read file and calculate entries
-            _dictionary.Clear();
-            foreach (var s in _text)
+            try
             {
-                if (!char.IsSeparator(s) && !char.IsWhiteSpace(s) && !char.IsPunctuation(s))
+                // Read file and calculate entries
+                _dictionary.Clear();
+                progressBar.Value = 0;
+                progressBar.Maximum = _text.Length;
+                await Task.Run(() =>
                 {
-                    var n = _text.Where(r => r == s).Count();
-                    if (!_dictionary.Keys.Contains(s))
+                    foreach (var s in _text)
                     {
-                        _dictionary.Add(s, n);
+                        Dispatcher.Invoke(() => { progressBar.Value++; });
+                        if (!char.IsSeparator(s) && !char.IsWhiteSpace(s) && !char.IsPunctuation(s))
+                        {
+                            var n = _text.Where(r => r == s).Count();
+                            if (!_dictionary.Keys.Contains(s))
+                            {
+                                _dictionary.Add(s, n);
+                            }
+                        }
                     }
-                }
+                });
+            }
+            catch
+            {
+
             }
         }
     }
